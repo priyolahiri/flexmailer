9c71bf2bab36040f182f479c67694388c7291371
Priyadarshi Lahiri <me@priyolahiri.co.cc>
2012-01-26 15:38:44 +0530

    Added Laravel and HTML Kickstart. Initialized base template.

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,122 +1,55 @@
-<!doctype html>
-<html>
-	<head>
-		<meta charset="utf-8">
+<!DOCTYPE html>
+<html><head>
+<title>HTML KickStart</title>
+<meta charset="UTF-8">
+<meta name="description" content="" />
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+<script type="text/javascript" src="js/jquery.snippet.min.js"></script>                         <!-- SNIPPET -->
+<script type="text/javascript" src="js/kickstart.js"></script>                                  <!-- KICKSTART -->
+<link rel="stylesheet" type="text/css" href="css/kickstart.css" media="all" />                  <!-- KICKSTART -->
+<link rel="stylesheet" type="text/css" href="css/style.css" media="all" />                          <!-- CUSTOM STYLES -->
+</head><body><a id="top-of-page"></a><div id="wrap" class="clearfix">
+<!-- ===================================== END HEADER ===================================== -->
 
-		<title>Laravel - A Framework For Web Artisans</title>
 
-		<style>
-			@import url(http://fonts.googleapis.com/css?family=Ubuntu);
+	<!-- 
+	
+		ADD YOU HTML ELEMENTS HERE
+		
+		Example: 2 Columns
+	 -->
+	 
+	 <div class="col_12">
+	 <ul class="breadcrumbs alt1">
+	 	<li><a href="">Home</a></li>
+	 	<li><a href="">Flexmailer</a></li>
+	 </ul>
+	 </div>
+	 
+	 <div class="col_9">
+	 <h3>Welcome to Flexmailer</h3>
+	 <p>Please log in from the sidebar to access the system. Please use it responsibly.</p>
+	 </div>
+	 
+	 <div class="col_3">
+	 <h5>Login</h5>
+	 <form method="post" action="/login">
+	 	<label for="username">Username</label>
+    	<input id="username" name="username" type="text" />
+    	<label for="password">Password</label>
+    	<input id="password" name="password" type="text" />
+    	<button type="submit" class="small green">Log Me In</button>
+	 </form>
+	 </div>
 
-			body {
-				background: #eee;
-				color: #6d6d6d;
-				font: normal normal normal 14px/1.253 Ubuntu, sans-serif;
-				margin: 0 0 25px 0;
-				min-width: 800px;
-				padding: 0;
-			}
 
-			#main {
-				background-clip: padding-box;
-				background-color: #fff;
-				border:1px solid #ccc;
-				border-radius: 5px;
-				box-shadow: 0 0 10px #cdcdcd;
-				margin: 25px auto 0;
-				padding: 30px;
-				width: 700px;
-				position: relative;
-			}
+<!-- ===================================== START FOOTER ===================================== -->
+<div class="clear"></div>
+<div id="footer">
+&copy; Copyright 2011–2012 All Rights Reserved. This website was built with <a href="http://www.99lime.com">HTML KickStart</a>
+<a id="link-top" href="#top-of-page">Top</a>
+</div>
 
-			#main h1 {
-				font-family: 'Ubuntu';
-				font-size: 38px;
-				letter-spacing: 2px;
-				margin: 0 0 10px 0;
-				padding: 0;
-			}
-
-			#main h2 {
-				color: #999;
-				font-size: 18px;
-				letter-spacing: 3px;
-				margin: 0 0 25px 0;
-				padding: 0 0 0 0;
-			}
-
-			#main h3 {
-				color: #999;
-				margin-top: 24px;
-				padding: 0 0 0 0;
-			}
-
-			#main h3 {
-				font-size: 18px;
-			}
-
-			#main p {
-				line-height: 25px;
-				margin: 10px 0;
-			}
-
-			#main pre {
-				background-color: #333;
-				border-left: 1px solid #d8d8d8;
-				border-top: 1px solid #d8d8d8;
-				border-radius: 5px;
-				color: #eee;
-				padding: 10px;
-			}
-
-			#main ul {
-				margin: 10px 0;
-				padding: 0 30px;
-			}
-
-			#main li {
-				margin: 5px 0;
-			}
-		</style>
-	</head>
-	<body>
-		<div id="main">
-			<h1>Welcome To Laravel</h1>
-
-			<h2>A Framework For Web Artisans</h2>
-
-			<p>
-				You have successfully installed the Laravel framework. Laravel is a simple framework
-				that helps web artisans create beautiful, creative applications using elegant, expressive
-				syntax. You'll love using it.
-			</p>
-
-			<h3>Learn the terrain.</h3>
-
-			<p>
-				You've landed yourself on our default home page. The route that
-				is generating this page lives at:
-			</p>
-
-			<pre><code>APP_PATH/routes.php</code></pre>
-
-			<p>And the view sitting before you can be found at:</p>
-
-			<pre><code>APP_PATH/views/home/index.php</code></pre>
-
-			<h3>Create something beautiful.</h3>
-
-			<p>
-				Now that you're up and running, it's time to start creating!
-				Here are some links to help you get started:
-			</p>
-
-			<ul>
-				<li><a href="http://laravel.com">Official Website</a></li>
-				<li><a href="http://forums.laravel.com">Laravel Forums</a></li>
-				<li><a href="http://github.com/laravel/laravel">GitHub Repository</a></li>
-			</ul>
-
-		</div>
-	</body>
-</html>
+</div><!-- END WRAP -->
+</body></html>
